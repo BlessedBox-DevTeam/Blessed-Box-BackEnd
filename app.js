f31e628a9b27f5b19b46d094dcc9c55a11e87ea0
Kwilliams4 <136720089+Kwilliams4@users.noreply.github.com>
2025-08-07 16:52:26 +0000

    debug

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const socketSetup = require('./socket.js');
 const db = require('./db.js');
 const path = require('path');
-require('dotenv').config({
-  path: path.resolve(__dirname, '.env'),
-});
+const fs = require('fs');
+const envPath = path.resolve(__dirname, '.env');
 
-console.log('__dirname:', __dirname);
-console.log('process.env.PORT:', process.env.PORT);
+console.log('Existe .env:', fs.existsSync(envPath));
+console.log('Contenido del .env:\n', fs.readFileSync(envPath, 'utf-8'));
+
+require('dotenv').config({ path: envPath });
+
+console.log('process.env.PORT después de dotenv:', process.env.PORT);
 
 
 
