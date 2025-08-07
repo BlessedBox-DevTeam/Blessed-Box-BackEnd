278fd1b7abbfbde1faba578012a7ef713fe0700a
Kwilliams4 <136720089+Kwilliams4@users.noreply.github.com>
2025-08-07 17:02:46 +0000

    debug

--- app.js
+++ app.js
@@ -33,3 +33,7 @@ const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Servidor funcionando correctamente' });
+});
