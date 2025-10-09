70808e74d82a6872ff3e32d6d2134298474d10f8
Kwilliams4 <136720089+Kwilliams4@users.noreply.github.com>
2025-10-09 13:49:51 -0400

    Update and rename RecolectionCenter.js to RecollectionCenter.js

---
+++ models/RecollectionCenter.js
@@ -0,0 +1,33 @@
+const db = require("../db.js");
+
+// Insert a new recollection center
+const newRecollectionCenter = async (name, countryId, location, qrCodeId) => {
+  const [result] = await db.query(
+    "INSERT INTO recollectionCenters 
+    (recollectionCenterName, countryId, location, qrCodeId) 
+    VALUES (?, ?, ?, ?)",
+    [name, countryId, location, qrCodeId]
+  );
+  return result;
+};
+
+// Get all recolection centers
+const getRecollectionCenters = async () => {
+  const [rows] = await db.query("SELECT * FROM recollectionCenters");
+  return rows;
+};
+
+// Get a recolection center by ID
+const getRecollectionCenterById = async (id) => {
+  const [rows] = await db.query(
+    "SELECT * FROM recollectionCenters WHERE id = ?",
+    [id]
+  );
+  return rows[0] || null;
+};
+
+module.exports = {
+  newRecollectionCenter,
+  getRecollectionCenters,
+  getRecollectionCenterById
+};
