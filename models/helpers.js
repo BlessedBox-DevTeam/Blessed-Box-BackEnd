6f89d7e1f37ad0d57311f0e8e24440e6c25a33b1
Kwilliams4 <136720089+Kwilliams4@users.noreply.github.com>
2025-10-16 06:20:13 -0400

    Add returnServiceObject function to helpers

---
+++ models/helpers.js
@@ -0,0 +1,10 @@
+function returnServiceObject({ success = false, data = null, message = "", error = null }) {
+    return {
+        success,
+        data,
+        message,
+        error
+    };
+}
+
+module.exports = returnServiceObject;
