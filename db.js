b282001ddc16d65ddb80ff8f8c51d5f47c024736
Kenneth Williams <kennepo23@gmail.com>
2026-08-04 06:08:51 -0400

    Removed path from db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-const path = require("path");
 const dotenv = require("dotenv");
+dotenv.config();
 const mysql = require("mysql2/promise");
 
 // Cargar variables antes de usar
 const env = process.env.NODE_ENV || "development";
-dotenv.config({ path: path.resolve(process.cwd(), `.env.${env}`) });
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
