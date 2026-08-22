d2918dd03cc8dfdf45b9fc13d1666197fc21189c
Kenneth Williams <kennepo23@gmail.com>
2026-08-22 13:00:34 -0400

    Added error handling for controller and refactor query.

--- controllers/boxController.js
+++ controllers/boxController.js
@@ -4,33 +4,12 @@ const {
   getDepositedBoxesCountByUserId,
   getBoxesCountByRecollectionCenterId
 } = require("../models/Box");
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
-
-if (!JWT_SECRET || !JWT_REFRESH_SECRET) {
-  throw new Error(
-    "JWT_SECRET o JWT_REFRESH_SECRET no están definidos. Configura tu archivo .env"
-  );
-}
 
 async function getUserDepositedBoxes(req, res) {
   const conn = await db.getConnection();
-  const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ message: "Missing token" });
+  const { userId } = req?.user;
 
-  const token = authHeader.split(" ")[1];
-  let payload;
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid or expired access token" });
-  }
-
-  const userBoxesResponse = await getDepositedBoxesCountByUserId(
-    payload.userId,
-    conn
-  );
+  const userBoxesResponse = await getDepositedBoxesCountByUserId(userId, conn);
   if (!userBoxesResponse.success) {
     return res.status(500).json({
       message: "Internal server. Error on finding user deposited boxes"
@@ -44,16 +23,6 @@ async function getUserDepositedBoxes(req, res) {
 }
 async function getBoxesCountByRecollectionCenter(req, res) {
   const conn = await db.getConnection();
-  const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ message: "Missing token" });
-  const token = authHeader.split(" ")[1];
-
-  let payload;
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid or expired access token" });
-  }
 
   const boxesByRCResponse = await getBoxesCountByRecollectionCenterId(
     BETHLEHEM_RECOLLECTION_CENTER_ID,
