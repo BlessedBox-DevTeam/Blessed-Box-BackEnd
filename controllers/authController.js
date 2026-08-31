e101412849e5f1e93593c450d51d5309c4df42da
Kenneth Williams <kennepo23@gmail.com>
2026-08-31 05:42:39 -0400

    Added log.

--- controllers/authController.js
+++ controllers/authController.js
@@ -202,7 +202,6 @@ async function verifyOtp(req, res) {
     const { valid, normalizedEmail } = validateEmail(email);
 
     if (!valid || !otp) {
-      console.log("isvalid", valid);
       return res.status(400).json({ error: "Email y OTP son obligatorios." });
     }
 
--- db.js
+++ db.js
@@ -12,7 +12,15 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  typeCast: function (field, next) {
+    // Si el tipo es BIT y solo mide 1 bit de longitud
+    if (field.type === "BIT" && field.length === 1) {
+      const bytes = field.buffer();
+      return bytes === null ? null : bytes[0] === 1;
+    }
+    return next();
+  }
 });
 
 module.exports = db;
