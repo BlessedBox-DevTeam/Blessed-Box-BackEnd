410231c28eb2f3c17507936e71185c1bb6d8f4e8
Kenneth Williams <kennepo23@gmail.com>
2026-09-04 15:57:09 -0400

    Added log

--- models/Transaction.js
+++ models/Transaction.js
@@ -287,7 +287,7 @@ const generateTransactionNumber = async (conn) => {
     ORDER BY id DESC
     LIMIT 1`
     );
-    const id = rows[0].id ?? 0;
+    const id = rows[0]?.id ?? 0;
     return returnServiceObject({
       success: true,
       data: `BBX-${new Date().getFullYear()}-${String(id).padStart(6, "0")}`
