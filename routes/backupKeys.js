a75ab5bede2e63a197598fef31997a889b985165
Kenneth Williams <kennepo23@gmail.com>
2026-09-18 06:47:24 -0400

    Added middlewares for backupkey route.

--- routes/backupKeys.js
+++ routes/backupKeys.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const backupKeyController = require("../controllers/backupKeyController");
+const { authenticate } = require("../middleware/authenticate");
+const { authorize } = require("../middleware/authorize");
+const permissions = require("../helpers/constants");
 
-router.post("/isKey", backupKeyController.isKeyCorrect);
+router.post(
+  "/isKey",
+  authenticate,
+  authorize([permissions.WRITE_TRANSACTION_PERMISSION]),
+  backupKeyController.isKeyCorrect
+);
 
 module.exports = router;
