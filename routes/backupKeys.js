90856a6d174e6af09f47c87b4899d687d9e8df92
Kenneth Williams <kennepo23@gmail.com>
2026-09-20 20:49:09 -0400

    Added rate limit to endpoint.

--- routes/backupKeys.js
+++ routes/backupKeys.js
@@ -5,9 +5,15 @@ const { authenticate } = require("../middleware/authenticate");
 const { authorize } = require("../middleware/authorize");
 const { rateLimitAccessCode } = require("../middleware/accessCodeRateLimit");
 const permissions = require("../helpers/constants");
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
 
 router.post(
   "/isKey",
+  limiter,
   authenticate,
   authorize([permissions.WRITE_TRANSACTION_PERMISSION]),
   rateLimitAccessCode,
